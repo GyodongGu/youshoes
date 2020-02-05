e3545c4aa90ef174401ee184bd2fc106a9c107ac
user <user@YD03-16>
2020-02-05 14:08:37 +0900

    2020-02-05

---
+++ youshoes/src/shoes/controller/FrontController.java
@@ -0,0 +1,77 @@
+package shoes.controller;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import shoes.command.Command;
+
+/**
+ * Servlet implementation class FrontController
+ */
+@WebServlet("/FrontController")
+public class FrontController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	HashMap<String, Command> list =null;   
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public FrontController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+		list=new HashMap<String, Command>();
+//		list.put("", value);
+	}
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.setCharacterEncoding("utf-8");
+		String uri = request.getRequestURI();
+		String context = request.getContextPath();
+		String path = uri.substring(context.length());
+		
+		Command commandImpl = list.get(path);
+		
+		String page = null;
+		
+		response.setContentType("text/html; charset=UTF-8");
+		
+		if(commandImpl!=null) {
+			page = commandImpl.execute(request, response);
+			
+			if(page!=null && !page.isEmpty()) {
+				if(page.startsWith("redirect:")) {
+					String view = page.substring(9);
+					response.sendRedirect(view);
+				}else if(page.startsWith("ajax:")) {
+					response.getWriter().append(page.substring(5));
+				}else if(page.startsWith("script:")) {
+					response.getWriter().append("<script>").append(page.substring(7)).append("</script>");
+				}else {
+					request.getRequestDispatcher(page).forward(request, response);
+				}
+			}else {
+				response.getWriter().append("잘못된 요청입니다.");
+			}
+
+		}
+	}
+
+}
---
+++ youshoes/src/shoes/dao/DAO.java
@@ -0,0 +1,36 @@
+package shoes.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+public class DAO {
+	protected Connection conn;
+	protected PreparedStatement pstmt;
+	protected ResultSet rs;
+	protected DataSource ds;
+	
+	private String driver = "oracle.jdbc.driver.OracleDriver";
+	private String url = "jdbc:oracle:thin:@localhost:1521:xe";
+	private String user = "hr";
+	private String pw = "hr";
+	
+	public DAO() {
+		try {
+			Class.forName(driver);
+			conn=DriverManager.getConnection(url, user, pw);
+		} catch (ClassNotFoundException | SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void close() {
+		
+	}
+	
+}
