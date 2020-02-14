f3896cc9bdbc8cd3c94f62b6058b98c49eb83012
User <User@YD03-20>
2020-02-14 15:35:27 +0900

    배광준:로그인

--- youshoes/src/shoes/command/loginOkCommand.java
+++ youshoes/src/shoes/command/loginOkCommand.java
@@ -1,6 +1,7 @@
 package shoes.command;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -25,8 +26,11 @@ public class loginOkCommand implements Command {
 		String grant = dao.loginCheck(id, pw);
 
 		if (grant == null) {   // id와 pw가 일치하지않다면
-			response.sendRedirect("view/login.jsp");
-			
+			response.setContentType("text/html; charset=UTF-8");
+			PrintWriter out = response.getWriter();
+			out.println("<script>alert('아이디/비밀번호를 알맞게 입력하세요.'); location.href='view/login.jsp';</script>");
+			out.flush();
+
 		} else {   	// 로그인 성공하면 (id와 pw가 일치) ""안의 페이지로 이동
 			HttpSession httpsession = request.getSession(); // 자바객체를 이용해서 session객체를 이용할 떄
 			httpsession.setAttribute("pm_id", id);
