bc45f4787684cd375d9583d225cd4b5c7f6ba4fa
dhktjdyd12 <55132488+dhktjdyd12@users.noreply.github.com>
2020-02-18 10:07:36 +0900

    2020.02.18 유승우 통합 오류 수정 2

--- youshoes/src/shoes/command/logoutCommand.java
+++ youshoes/src/shoes/command/logoutCommand.java
@@ -2,11 +2,9 @@ package shoes.command;
 
 import java.io.IOException;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import shoes.common.Command;
 
@@ -15,10 +13,7 @@ public class logoutCommand implements Command {
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-
-		return "/login.do";
-
-//		return "view/pMem/logout.jsp";
+		return "view/pMem/logout.jsp";
 	}
 
 }
