28715581dfca14ea5d43035ceba061314496c7ad
user <user@YD03-16>
2020-02-20 17:07:02 +0900

    2020-02-20 구교동 dto수정

--- youshoes/src/shoes/dto/reservationDTO.java
+++ youshoes/src/shoes/dto/reservationDTO.java
@@ -4,10 +4,11 @@ import java.sql.Date;
 
 public class reservationDTO {
 	private int res_no;
-	private int ord_no;
+	
 	private String sm_id;
 	private Date res_date;
 	private String res_remark;
+	private int pm_no;
 	
 	public int getRes_no() {
 		return res_no;
@@ -16,11 +17,12 @@ public class reservationDTO {
 		this.res_no = res_no;
 	}
 
-	public int getOrd_no() {
-		return ord_no;
+
+	public int getPm_no() {
+		return pm_no;
 	}
-	public void setOrd_no(int ord_no) {
-		this.ord_no = ord_no;
+	public void setPm_no(int pm_no) {
+		this.pm_no = pm_no;
 	}
 	public String getSm_id() {
 		return sm_id;
@@ -43,10 +45,9 @@ public class reservationDTO {
 	
 	@Override
 	public String toString() {
-		return "reservationDTO [res_no=" + res_no + ", ord_no=" + ord_no + ", sm_id=" + sm_id + ", res_date=" + res_date
-				+ ", res_remark=" + res_remark + "]";
+		return "reservationDTO [res_no=" + res_no + ", sm_id=" + sm_id + ", res_date=" + res_date + ", res_remark="
+				+ res_remark + ", pm_no=" + pm_no + "]";
 	}
-	
 
 	
 }
