717d7d286dfb2d31d1b8e9ce42ed48e6b23a4874
TFX255 <TFX255@DESKTOP-B9SEJ1R>
2020-03-24 12:24:22 +0900

    03/24 12:24 권우성 고객페이지 캘린더 수정

--- youshoes/src/shoes/command/SetReservCommand.java
+++ youshoes/src/shoes/command/SetReservCommand.java
@@ -20,30 +20,10 @@ public class SetReservCommand implements Command {
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-			
-			HttpSession session = request.getSession(false);
-			String id = (String) session.getAttribute("id");
-			pmDTO dto = new pmDTO();
-			ProductOrderDAO dao = new ProductOrderDAO();
-			dto = dao.select(id);	
-		
-			
-			CallenderDAO dao1 = new CallenderDAO();
-			reservationDTO dto1 = new reservationDTO();
+
 			String reserv1 = request.getParameter("res_date");
 			String smid = request.getParameter("sm_id");
-			/**
-			int pmno = dto.getPm_no();
-			try {
-				dto1.setRes_date(new java.sql.Date(new SimpleDateFormat("yyyy-MM-dd hh:mm").parse(reserv1).getTime()));
-				dto1.setSm_id(smid);
-				dto1.setPm_no(pmno);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-			int r;
-			r=dao1.insertform(dto1);
-			**/
+
 		return "ajax:" + reserv1;
 	}
 
